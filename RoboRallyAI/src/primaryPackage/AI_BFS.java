67498e7587b7e467070c34212630f8b406565734
Erick <Erick@DESKTOP-L0OT4CP.clients.wireless.dtu.dk>
2019-03-19 11:22:26 +0100

    Last bit of BFS implemented - frontier search and visiting.

--- RoboRallyAI/src/primaryPackage/AI_BFS.java
+++ RoboRallyAI/src/primaryPackage/AI_BFS.java
@@ -1,23 +1,25 @@
 package primaryPackage;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Queue;
 
 public class AI_BFS implements AI_Interface {
 	
-	Queue<AI_State> queue = new ArrayDeque<AI_State> ();
+	Queue<AI_State> frontier = new ArrayDeque<AI_State> ();
+	ArrayList<AI_State> visited = new ArrayList<AI_State> ();
 	Board board;
 	
 	// Create this algorithm class.
 	public AI_BFS(Board board) {
 		this.board = board;
+		// Initialize the first state.
 		createNewState();
 	}
 	
-	// Pulls out the top of the state queue and initializes it into our board.
-	private void retrieveState() {
+	// Loads all robots into the board from a state.
+	private void loadState(AI_State state) {
 		board.clearRobots();
-		AI_State state = queue.poll();
 		for (Robot robot : state.robots) {
 			board.createRobot(robot.colour, robot.x, robot.y);
 		}
@@ -33,14 +35,51 @@ public class AI_BFS implements AI_Interface {
 			newRobot.y = robot.y;
 			state.robots.add(newRobot);
 		}
-		queue.add(state);
+		frontier.add(state);
 	}
 
 	// The great algorithm itself.
 	@Override
 	public String Search() {
-		
-		return null;
+		AI_State currentState = new AI_State();
+		// While we are not in a goal state, we keep searching.
+		while (frontier.peek() != null) {
+			if (!visited.contains(frontier.peek())) { // Make sure we haven't already visited this state.
+				currentState = frontier.poll(); // Poll top of queue.
+				loadState(currentState); // Load current state's robots into the board.
+				visited.add(currentState); // Visit current state.
+				if (board.checkGoal()) { // Let's see if we've found our goal.
+					return "Goal found!";
+				} else {
+					checkFrontier(currentState); // Visit all states from current state. This is a larger method.
+				}
+			}
+		}
+		return "No goal could be found.";
 	}
 
+	// Check all children states and add them to frontier.
+	private void checkFrontier(AI_State currentState) {
+		// For each robot, perform all movement
+		for (Robot robot : board.robots) {
+			for (int i = 0; i < 4; i++) { // Four iterations - one per movement available.
+				loadState(currentState);
+				switch (i) {
+				case 0:
+					board.moveRobot(robot.colour, "up");
+					break;
+				case 1:
+					board.moveRobot(robot.colour, "down");
+					break;
+				case 2:
+					board.moveRobot(robot.colour, "left");
+					break;
+				case 3:
+					board.moveRobot(robot.colour, "right");
+					break;
+				}
+				createNewState();
+			}
+		}
+	}
 }
--- RoboRallyAI/src/primaryPackage/AI_State.java
+++ RoboRallyAI/src/primaryPackage/AI_State.java
@@ -2,6 +2,19 @@ package primaryPackage;
 
 import java.util.ArrayList;
 
-public class AI_State {
+public class AI_State implements Comparable<AI_State> {
 	ArrayList<Robot> robots = new ArrayList<Robot> ();
+	String commandline = ""; // The commands on robots made to reach this state.
+	
+	
+	@Override
+	public int compareTo(AI_State o) {
+		if (robots.equals(o.robots)) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	
+
 }
